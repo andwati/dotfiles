4a4b818362e46c7c64e71d4bd7cfb35f0d3bbfcc
Ian Andwati <andwatiian@gmail.com>
2022-07-13 21:02:36 +0300

    completed kde plasma  install script

--- arch-install/base-uefi.sh
+++ arch-install/base-uefi.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 
 # Install essential packages
-pacstrap /mnt base linux linux-firmware grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font nano
+pacstrap /mnt base linux linux-firmware  intel-ucode vim grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld  sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font nano
 
 # Configure the system
 
