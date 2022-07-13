609950b444755b597fc2a3f9aa7ebff0b1281325
Ian Andwati <andwatiian@gmail.com>
2022-07-13 14:49:29 +0300

    added git configuration

---
+++ git/setup.sh
@@ -0,0 +1,7 @@
+# first time git setup
+git config --global user.name "Ian Andwati"
+git config --global user.email "andwatiian@gmail.com"
+git config --global init.defaultBranch main
+
+# :) because why not?
+git config --global core.editor nano
