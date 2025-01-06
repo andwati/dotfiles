d3cdac9252ae8f4c1f485e2bd60286700b67950f
Ian Andwati <andwatiian@gmail.com>
2025-01-06 23:26:29 +0300

    Update deps.sh

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -3,7 +3,7 @@ sudo pacman -Syu
 
 sudo pacman -Sy --needed base-devel gcc clang llvm rustup  libxau libxi libxss libxtst libxcursor libxcomposite libxdamage libxfixes libxrandr libxrender mesa-libgl alsa-lib libglvnd
 
-sudo pacman -Sy --needed grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils  bash-completion openssh rsync virt-manager qemu bridge-utils dnsmasq vde2 openbsd-netcat  ipset  sof-firmware nss-mdns acpid os-prober ntfs-3g docker minikube kubectl zig zls  qbittorrent git python-pip python-pipx python-pipenv python-poetry firefox go  tor torbrowser-launcher gimp kdenlive vlc postgresql 
+sudo pacman -Sy --needed grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools base-devel  avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils  bash-completion openssh rsync virt-manager qemu bridge-utils dnsmasq vde2 openbsd-netcat  ipset  sof-firmware nss-mdns acpid os-prober ntfs-3g docker minikube kubectl zig zls  qbittorrent git python-pip python-pipx python-pipenv python-poetry firefox go  tor torbrowser-launcher gimp kdenlive vlc postgresql 
 
 
 rustup toolchain add stable
