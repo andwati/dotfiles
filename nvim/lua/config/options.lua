e87c64c3bef2d12d740f8501b471ecc70d64c560
andwati <andwatiian@gmail.com>
2026-05-19 12:59:43 +0300

    cleanup

--- nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
-vim.opt.number = true
-vim.opt.cursorline = true
-vim.opt.relativenumber = true
-vim.opt.shiftwidth = 4
-
-
-
--- scripts/mirrors.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
-sudo apt autoclean -y
-
-# this fixes .ssh and .gnupg directory permissions
-find ~/.gnupg -type d -exec chmod 700 {} \;
-find ~/.gnupg -type f -exec chmod 600 {} \;
-chmod 600 ~/.ssh/* && chmod 700 ~/.ssh && chmod 644 ~/.ssh/*.pub
